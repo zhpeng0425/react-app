650a757e3f9666114db123ad8713c9507a8501da
zhpeng0425 <12118716+zhpeng0425@users.noreply.github.com>
2025-03-27 17:37:53 +0800

    Add files via upload

--- src/ToDoApp/ToDoApp.jsx
+++ src/ToDoApp/ToDoApp.jsx
@@ -1,125 +1,57 @@
 import React, { useState } from 'react';
-import ToDoList from './components/ToDoList';
+import ToDoList from './components/ToDoList.jsx';
+import ToDoItemEditModal from './components/ToDoItemEditModal.jsx';
+import './ToDoApp.css';
 
 function ToDoApp() {
-<<<<<<< HEAD
+  const [addModalVisible, setAddModalVisible] = useState(false);
+
   const toDoListData = [
     { id: 1, text: '待办事项1', level: 1, isDone: true },
     { id: 2, text: '待办事项2', level: 2, isDone: false },
     { id: 3, text: '待办事项3', level: 3, isDone: false },
     { id: 4, text: '待办事项4', isDone: false },
   ];
-
   const [list, setList] = useState(toDoListData);
 
-  const handleClick = (id) => {
+  const handleItemClick = (id) => {
     const clickedItem = list.find((item) => item.id === id);
     clickedItem.isDone = !clickedItem.isDone;
     setList([...list]);
   };
 
+  const handleAddItemClick = () => {
+    setAddModalVisible(true);
+  };
+
+  const handleAddItemOkClick = (newItem) => {
+    let maxId = 0;
+    list.map((item) => {
+      if (item.id > maxId) {
+        maxId = item.id;
+      }
+    });
+
+    setList([...list, { id: maxId + 1, ...newItem, isDone: false }]);
+    setAddModalVisible(false);
+  };
+
   return (
     <>
-      <RenderDemo />
-      <ToDoList title="待办" list={list.filter((item) => !item.isDone)} onItemClick={handleClick} />
-      <ToDoList title="已办" list={list.filter((item) => item.isDone)} onItemClick={handleClick} />
-    </>
-  );
-}
-
-export default ToDoApp;
-
-function RenderDemo() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div style={{ display: 'flex' }}>
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>增加</button>
+      <div className="container">
+        <ToDoList title="待办" list={list.filter((item) => !item.isDone)} onItemClick={handleItemClick} />
+        <button className="btn-add" onClick={handleAddItemClick}>
+          添加事项
+        </button>
+        <ToDoList title="已办" list={list.filter((item) => item.isDone)} onItemClick={handleItemClick} />
       </div>
-      <input />
-    </>
-  );
-}
-=======
-  return (
-    <div className="container">
-      <section className="to-do-list-section">
-        <ToDoListHeader title="待办" />
-        <ToDoList />
-      </section>
-      <section className="to-do-list-section">
-        <ToDoListHeader title="已办" />
-        <ToDoList />
-      </section>
-    </div>
-  );
-}
-
-function ToDoListHeader({ title }) {
-  return (
-    <div className="to-do-list-header">
-      <div className="to-do-list-title">{title}</div>
-    </div>
-  );
-}
-
-function ToDoList() {
-  const toDoListData = [
-    { id: 1, text: '待办事项1', level: 1, isDone: true },
-    { id: 2, text: '待办事项2', level: 2, isDone: false },
-    { id: 3, text: '待办事项3', level: 3, isDone: false },
-    { id: 4, text: '待办事项4', isDone: false },
-  ];
-  const [list, setList] = useState(toDoListData);
-
-  const handleClick = (id) => {
-    const clickedItem = list.find((item) => item.id === id);
-    clickedItem.isDone = !clickedItem.isDone;
-    setList([...list]);
-  };
-
-  return (
-    <div className="to-do-list">
-      {list.map((item) => {
-        return <ToDoListItem key={item.id} item={item} onClick={handleClick} />;
-      })}
-    </div>
-  );
-}
-
-function ToDoListItem({ item, onClick }) {
-  let className = 'to-do-item';
-  switch (item.level) {
-    case 1:
-      className += ' level1';
-      break;
-    case 2:
-      className += ' level2';
-      break;
-    case 3:
-      className += ' level3';
-      break;
-    default:
-      break;
-  }
-  if (item.isDone) {
-    className += ' done';
-  }
-  return (
-    <label className={className}>
-      <input
-        type="checkbox"
-        checked={item.isDone}
-        onChange={() => {
-          onClick(item.id);
-        }}
+      <ToDoItemEditModal
+        visible={addModalVisible}
+        onCancelClick={() => setAddModalVisible(false)}
+        onOkClick={handleAddItemOkClick}
       />
-      <span>{item.text}</span>
-    </label>
+    </>
   );
 }
 
 export default ToDoApp;
->>>>>>> 04988686d18ee9897ab8a834cda3fab60732bec6
